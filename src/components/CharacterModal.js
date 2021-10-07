950acc323e854b963c992aa708ff499f79aa72c1
Mariia Paraketsova <paraketsova@gmail.com>
2021-10-07 21:24:28 +0200

    Style CharacterModal

--- src/components/CharacterModal.js
+++ src/components/CharacterModal.js
@@ -2,33 +2,41 @@ function CharacterModal({ data, closeFn }) {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <table>
-          <thead>
-          <tr>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Birth year</th>
-            <th>Height</th>
-            <th>Mass</th>
-            <th>Hair color</th>
-            <th>Skin color</th>
-            <th>Eye color</th>
-          </tr>
-          </thead>
+        <h1>{data.name}</h1>
 
+        <table className="modal-table">
           <tbody>
-          <tr>
-            <td>{data.name}</td>
-            <td>{data.gender}</td>
-            <td>{data.birth_year}</td>
-            <td>{data.height}</td>
-            <td>{data.mass}</td>
-            <td>{data.hair_color}</td>
-            <td>{data.skin_color}</td>
-            <td>{data.eye_color}</td>
-          </tr>
+            <tr>
+              <th>Gender</th>
+              <td>{data.gender}</td>
+            </tr>
+            <tr>
+              <th>Birth year</th>
+              <td>{data.birth_year}</td>
+            </tr>
+            <tr>
+              <th>Height</th>
+              <td>{data.height}</td>
+            </tr>
+            <tr>
+              <th>Mass</th>
+              <td>{data.mass}</td>
+            </tr>
+            <tr>
+              <th>Hair color</th>
+              <td>{data.hair_color}</td>
+            </tr>
+            <tr>
+              <th>Skin color</th>
+              <td>{data.skin_color}</td>
+            </tr>
+            <tr>
+              <th>Eye color</th>
+              <td>{data.eye_color}</td>
+            </tr>
           </tbody>
         </table>
+
         <button className="close-btn" onClick={closeFn}>Close</button>
       </div>
     </div>
